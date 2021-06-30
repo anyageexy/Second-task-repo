5666c5a1922980a925c0f329bc0e298ea34d95dc
anyageexy <anya.alexeeva2001@mail.ru>
2021-06-30 15:22:37 +0500

    adding plagins for styles in page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-// создаем элемент заголовка
-const heading = document.createElement('h1');
-heading.textContent = 'Как интересно!';
+import example from './images/response-image.png'
 
-// добавляем заголовок в DOM
-const root = document.querySelector('#root');
-root.append(heading);
+import './styles/main.scss'
+
+// создание свойства класса без конструктора
+class Game {
+    name = 'Violin Charades'
+}
+const myGame = new Game()
+
+// создаем параграф
+const p = document.createElement('p')
+p.textContent = `I like ${myGame.game}.`
+
+// создаем элемент заголовка
+const heading = document.createElement('h1')
+heading.textContent = 'Как интересно!'
+
+// добавляем параграф и заголовок в DOM
+const root = document.querySelector('#root')
+root.append(heading, p);
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
     entry: {
         main: path.resolve(__dirname, './src/index.js'),
     },
+    mode: 'development',
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].bundle.js',
@@ -16,6 +17,31 @@ module.exports = {
             template: path.resolve(__dirname, './src/main.html'), // шаблон
             filename: 'index.html', // название выходного файла
         }),
-        new CleanWebpackPlugin(),
+        new CleanWebpackPlugin()
     ],
+    module: {
+        rules: [
+            // JavaScript
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            // изображения
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            // шрифты и SVG
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+            // CSS, PostCSS, Sass
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+        ],
+    }
 }

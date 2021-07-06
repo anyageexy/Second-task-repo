33721d4721546bd11a33240a39476ff25fc3f095
anyageexy <anya.alexeeva2001@mail.ru>
2021-07-06 22:11:21 +0500

    creating mixin text-field and event handlers for clicking on the field and outside of it

---
+++ src/scripts/text-field.js
@@ -0,0 +1,17 @@
+document.addEventListener("click", (evt) => {
+  const flyoutElement = document.getElementById("text-field");
+  let targetElement = evt.target; // clicked element
+
+  do {
+      if (targetElement == flyoutElement) {
+          // Do nothing, just return.
+          document.getElementById("text-field").value = "This is pretty";
+          return;
+      }
+      // Go up the DOM.
+      targetElement = targetElement.parentNode;
+  } while (targetElement);
+
+  // Do something useful here.
+  document.getElementById("text-field").value = "";
+});

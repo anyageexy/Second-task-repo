663cfe7a603b751715299db8c111ce575f4fa270
anyageexy <anya.alexeeva2001@mail.ru>
2021-07-08 11:45:26 +0500

    adding functions for text-field on focus and blur

--- src/scripts/text-field.js
+++ src/scripts/text-field.js
@@ -1,4 +1,30 @@
+// function valueFocus(){
+//   document.getElementById('text-field').value = "First Name Only";
+// }
+
+// function valueBlur(){
+//   document.getElementById('text-field').value = "";
+// }
+
 document.addEventListener("click", (evt) => {
+  const flyoutElement = document.getElementById("text-field");
+  let targetElement = evt.target; // clicked element
+
+  do {
+      if (targetElement == flyoutElement) {
+          // Do nothing, just return.
+          document.getElementById("text-field").value = "This is pretty";
+          return;
+      }
+      // Go up the DOM.
+      targetElement = targetElement.parentNode;
+  } while (targetElement);
+
+  // Do something useful here.
+  document.getElementById("text-field").value = "";
+});
+
+document.addEventListener("mouseover", (evt) => {
   const flyoutElement = document.getElementById("text-field");
   let targetElement = evt.target; // clicked element
 

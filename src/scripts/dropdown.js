29c9b0cbfc1db486ae0eade10a36b2b59949a7d3
anyageexy <anya.alexeeva2001@mail.ru>
2021-07-15 19:39:23 +0500

    adding changing dropdown head function

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -9,47 +9,60 @@ function myFunction() {
   var head = document.querySelector('.dropdown-head');
   menu.classList.toggle('show');
   head.classList.toggle('dropdown-head-show');
- 
+}
+
+function main_counter(){
+  var head = document.querySelector('.head-text');
+  var counter_1 = document.querySelector('.span-count_1').innerHTML;
+  var counter_2 = document.querySelector('.span-count_2').innerHTML;
+  var counter_3 = document.querySelector('.span-count_3').innerHTML;
+  head.innerHTML = counter_1 + " спален, " + counter_2 + " кроватей, " + counter_3 + " ванных комнат";
 }
 
 function item_1_decrement(){
   var counter_1 = document.querySelector('.span-count_1');
   if(counter_1.innerHTML > 0){
     counter_1.innerHTML -= 1;
-  }
+  };
+  main_counter();
 }
 
 function item_1_increment(){
   var counter_1 = document.querySelector('.span-count_1');
   if(counter_1.innerHTML < 100){
     counter_1.innerHTML = parseInt(parseInt(counter_1.innerHTML)+1);
-  }
+  };
+  main_counter();
 }
 
 function item_2_decrement(){
   var counter_2 = document.querySelector('.span-count_2');
   if(counter_2.innerHTML > 0){
     counter_2.innerHTML -= 1;
-  }
+  };
+  main_counter();
 }
 
 function item_2_increment(){
   var counter_2 = document.querySelector('.span-count_2');
   if(counter_2.innerHTML < 100){
     counter_2.innerHTML = parseInt(parseInt(counter_2.innerHTML)+1);
-  }
+  };
+  main_counter();
 }
 
 function item_3_decrement(){
   var counter_3 = document.querySelector('.span-count_3');
   if(counter_3.innerHTML > 0){
     counter_3.innerHTML -= 1;
-  }
+  };
+  main_counter();
 }
 
 function item_3_increment(){
   var counter_3 = document.querySelector('.span-count_3');
   if(counter_3.innerHTML < 100){
     counter_3.innerHTML = parseInt(parseInt(counter_3.innerHTML)+1);
-  }
+  };
+  main_counter();
 }

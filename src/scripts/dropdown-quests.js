12342b1fa812efd63547c621ed8bb1c43d3cb8c1
anyageexy <anya.alexeeva2001@mail.ru>
2021-07-19 20:38:00 +0500

    adding functional for btn-clear

--- src/scripts/dropdown-quests.js
+++ src/scripts/dropdown-quests.js
@@ -4,7 +4,7 @@
 //     document.querySelector('.dropdown-list').classList.toggleClass('show');
 // }
 
-function myFunction_quests() {
+function my_Function_quests() {
   var menu = document.querySelector('.dropdown-quests-list'); // Using a class instead, see note below.
   var head = document.querySelector('.dropdown-quests-head');
   menu.classList.toggle('quests-show');
@@ -14,11 +14,20 @@ function btn_clear_show(){
   var btn = document.querySelector('.btn-clear');
   btn.classList.add('btn-clear-show');
 }
-function btn_clear_hidden(){
+function btn_clear_hidden(){///функция при скрытии кнопки очистить
   var btn = document.querySelector('.btn-clear');
   btn.classList.remove('btn-clear-show');
-  var head = document.querySelector('.dropdown-quests-head');
+  var head = document.querySelector('.quests-head-text');
   head.innerHTML = "Сколько гостей";
+  var labels = document.querySelectorAll(".quests-span-count");
+  labels.forEach(function(item){
+    item.innerHTML = "0";
+  })
+
+  highlightedItems.forEach(function(userItem) {
+  deleteUser(userItem);
+});
+
 }
 function main_counter_quests(){
   var head = document.querySelector('.quests-head-text');
@@ -30,55 +39,6 @@ function main_counter_quests(){
     head.innerHTML = head.innerHTML.substring(0, 20) + "...";
   }
 }
-
-function quests_item_1_decrement(){
-  var counter_1 = document.querySelector('.quests-span-count_1');
-  if(counter_1.innerHTML > 0){
-    counter_1.innerHTML -= 1;
-  };
-  main_counter_quests();
-}
-
-function quests_item_1_increment(){
-  var counter_1 = document.querySelector('.quests-span-count_1');
-  if(counter_1.innerHTML < 100){
-    counter_1.innerHTML = parseInt(parseInt(counter_1.innerHTML)+1);
-  };
-  main_counter_quests();
-}
-
-function quests_item_2_decrement(){
-  var counter_2 = document.querySelector('.quests-span-count_2');
-  if(counter_2.innerHTML > 0){
-    counter_2.innerHTML -= 1;
-  };
-  main_counter_quests();
-}
-
-function quests_item_2_increment(){
-  var counter_2 = document.querySelector('.quests-span-count_2');
-  if(counter_2.innerHTML < 100){
-    counter_2.innerHTML = parseInt(parseInt(counter_2.innerHTML)+1);
-  };
-  main_counter_quests();
-}
-
-function quests_item_3_decrement(){
-  var counter_3 = document.querySelector('.quests-span-count_3');
-  if(counter_3.innerHTML > 0){
-    counter_3.innerHTML -= 1;
-  };
-  main_counter_quests();
-}
-
-function quests_item_3_increment(){
-  var counter_3 = document.querySelector('.quests-span-count_3');
-  if(counter_3.innerHTML < 100){
-    counter_3.innerHTML = parseInt(parseInt(counter_3.innerHTML)+1);
-  };
-  main_counter_quests();
-}
-
 var string_quests_item = function(){
   var count_1 = document.querySelector('.quests-span-count_1').innerHTML;
   var count_2 = document.querySelector('.quests-span-count_2').innerHTML;
@@ -91,3 +51,47 @@ var string_quests_item = function(){
     return count_main + " гостя";
   } else return count_main + " гостей";
 }
+function quests_item_1_decrement(){
+  var counter_1 = document.querySelector('.quests-span-count_1');
+  if(counter_1.innerHTML > 0){
+    counter_1.innerHTML -= 1;
+  };
+  
+}
+
+function quests_item_1_increment(){
+  var counter_1 = document.querySelector('.quests-span-count_1');
+  if(counter_1.innerHTML < 100){
+    counter_1.innerHTML = parseInt(parseInt(counter_1.innerHTML)+1);
+  };
+  
+}
+
+function quests_item_2_decrement(){
+  var counter_2 = document.querySelector('.quests-span-count_2');
+  if(counter_2.innerHTML > 0){
+    counter_2.innerHTML -= 1;
+  };
+}
+
+function quests_item_2_increment(){
+  var counter_2 = document.querySelector('.quests-span-count_2');
+  if(counter_2.innerHTML < 100){
+    counter_2.innerHTML = parseInt(parseInt(counter_2.innerHTML)+1);
+  };
+}
+
+function quests_item_3_decrement(){
+  var counter_3 = document.querySelector('.quests-span-count_3');
+  if(counter_3.innerHTML > 0){
+    counter_3.innerHTML -= 1;
+  };
+}
+
+function quests_item_3_increment(){
+  var counter_3 = document.querySelector('.quests-span-count_3');
+  if(counter_3.innerHTML < 100){
+    counter_3.innerHTML = parseInt(parseInt(counter_3.innerHTML)+1);
+  };
+}
+

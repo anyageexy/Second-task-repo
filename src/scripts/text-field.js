a5c2ab7682beebb318e4b037c908fba2bac43b5c
anyageexy <anya.alexeeva2001@mail.ru>
2021-07-30 18:08:00 +0500

    no message

--- src/scripts/text-field.js
+++ src/scripts/text-field.js
@@ -1,14 +1,18 @@
+var counter_text_field = 0;
+var check_text_field = document.getElementById("text-field");
+
 document.addEventListener("click", (evt) => {
   const flyoutElement = document.getElementById("text-field");
-  let targetElement = evt.target; // clicked element
+  let targetElement = evt.target; // нажатый элемент
 
   do {
-    if (targetElement == flyoutElement) {
-      document.getElementById("text-field").value = "This is pretty";
-        return;
-    }
-    // Go up the DOM.
-    targetElement = targetElement.parentNode;
+      if (targetElement == flyoutElement) {
+          // Do nothing, just return.
+          document.getElementById("text-field").value = "This is pretty";
+          return;
+      }
+      // Go up the DOM.
+      targetElement = targetElement.parentNode;
   } while (targetElement);
 
   // Do something useful here.
@@ -17,18 +21,19 @@ document.addEventListener("click", (evt) => {
 
 document.addEventListener("mouseover", (evt) => {
   const flyoutElement = document.getElementById("text-field");
-  let targetElement = evt.target; // clicked element
+  let targetElement = evt.target; // нажатый элемент
 
   do {
-    if (targetElement == flyoutElement) {
-      // Do nothing, just return.
-      document.getElementById("text-field").value = "This is pretty";
-      return;
-    }
-    // Go up the DOM.
-    targetElement = targetElement.parentNode;
+      if (targetElement == flyoutElement) {
+          // Do nothing, just return.
+          document.getElementById("text-field").value = "This is pretty";
+          return;
+      }
+      // Go up the DOM.
+      targetElement = targetElement.parentNode;
   } while (targetElement);
 
   // Do something useful here.
-  document.getElementById("text-field").value = "";
+  document.getElementById("text-field").value = "";//когда уводим мышку то текстовое поле очищается
+  //нам нужно чтобы оно очищалось, если ничего не введено
 });
